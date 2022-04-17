6a7ff92a4ecaf97b0324177d2c476ba6340195fd
AndreyIvchenko <6491666@gmail.com>
2022-04-17 18:12:01 +0300

    task_one

--- JS_React/JS/script.js
+++ JS_React/JS/script.js
@@ -1,6 +1,21 @@
-// "use strict"; //строгий режим
+"use strict"; //строгий режим
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?',''); //+ useres answer number 
 
- const  isChecked=false,
-		isClose=false;
+//Object Date Base
+const personalMovieDB ={
+    count:numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false
+};
 
-console.log(isChecked||!isClose);
+const a=prompt('Один из просмотренных фильмов',''),
+	  b=prompt('Насколько оцените его?',''),
+	  c=prompt('Один из просмотренных фильмов',''),
+	  d=prompt('Насколько оцените его?','');
+
+personalMovieDB.movies[a]=b;
+personalMovieDB.movies[c]=d;
+
+console.log(personalMovieDB);
