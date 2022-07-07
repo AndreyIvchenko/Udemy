908ed30b7adb281d4fddfd7129e93860f51053df
AndreyIvchenko <6491666@gmail.com>
2022-07-07 10:28:57 +0300

    practic part 2 & puls project

--- JS_React/JS/script.js
+++ JS_React/JS/script.js
@@ -2,7 +2,7 @@
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?',''); //+ useres answer number 
 
 //Object Date Base
-const personalMovieDB ={
+const personalMovieDB = {
     count:numberOfFilms,
     movies:{},
     actors:{},
@@ -10,12 +10,33 @@ const personalMovieDB ={
     privat:false
 };
 
-const a=prompt('Один из просмотренных фильмов',''),
-	  b=prompt('Насколько оцените его?',''),
-	  c=prompt('Один из просмотренных фильмов',''),
-	  d=prompt('Насколько оцените его?','');
 
-personalMovieDB.movies[a]=b;
-personalMovieDB.movies[c]=d;
+for (let i=0; i < 2; i++) {
+	const a = prompt('Один из просмотренных фильмов',''),
+		b = prompt('Насколько оцените его?','');
+
+	if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50){
+		personalMovieDB.movies[a] = b;
+		console.log('done');
+	} else {
+		console.log('error');
+		i--; //retur back for one iteration 
+	}
+
+}
+
+//сделать двумя разными циклами** доп.задание
+if (personalMovieDB.count < 10) {
+	console.log('Просмотренно довольно мало фильмов');
+} else if (personalMovieDB.count < 30 && personalMovieDB.count >= 10) {
+	console.log('Вы классический зритель');
+} else if(personalMovieDB.count >= 30){
+	console.log ('Вы киноман');
+} else {
+	console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
+
+
+
