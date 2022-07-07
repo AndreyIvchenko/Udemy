42159922f73fcb98d1b68048403af411adc1c85a
AndreyIvchenko <6491666@gmail.com>
2022-07-07 10:39:38 +0300

    add files git json gulp

---
+++ js_task_udemy/js/script.js
@@ -0,0 +1,24 @@
+// "use strict"; //строгий режим
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?',''); //+ useres answer number 
+
+// //Object Date Base
+// const personalMovieDB ={
+//     count:numberOfFilms,
+//     movies:{},
+//     actors:{},
+//     genres:[],
+//     privat:false
+// };
+
+// const a=prompt('Один из просмотренных фильмов',''),
+// 	  b=prompt('Насколько оцените его?',''),
+// 	  c=prompt('Один из просмотренных фильмов',''),
+// 	  d=prompt('Насколько оцените его?','');
+
+// personalMovieDB.movies[a]=b;
+// personalMovieDB.movies[c]=d;
+
+// console.log(personalMovieDB);
+
+
+
